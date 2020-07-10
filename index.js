6a75ba4a973807fc70131ff8ebbf343cd4f03c55
kislev-owo <kislev_083@hotmail.com>
2020-07-10 19:26:29 +0000

    agregue impresion de la consola en js

--- index.js
+++ index.js
@@ -22,7 +22,18 @@ window.onload = () =>
     [Math.floor(Math.random()*accion.length)];	
     document.querySelector("#mensaje3").innerHTML = tiempo
     [Math.floor(Math.random()*tiempo.length)]; 
-	}
+
+    }
+    var person = document.querySelector("#mensaje1").innerHTML = personaje
+    [Math.floor(Math.random()*personaje.length)]; 
+    var accion1 = document.querySelector("#mensaje2").innerHTML = accion
+    [Math.floor(Math.random()*accion.length)];
+    var tiempo1 = document.querySelector("#mensaje3").innerHTML = tiempo
+    [Math.floor(Math.random()*tiempo.length)]; 
+    console.log(person);
+    console.log(accion1);
+    console.log(tiempo1);
+    
 }   //Math random para elegir un numero aleatorio en el array, 
     //Math Floor para que sea un numero entero,
     //length para tomar la longitud completa q esta adentro de nuestros arrays..
